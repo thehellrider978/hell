97dd2a341ed46712ae76b0e6987ea68c632e0a72
thehellrider978 <rajeshbajya978@gmail.com>
2025-01-30 04:42:49 +0530

    Create Phaser.js

---
+++ Phaser.js
@@ -0,0 +1,8 @@
+office-chair-racing/  
+├── assets/  
+│   ├── chairs/ (Player sprites)  
+│   ├── obstacles/ (Coffee, papers, etc.)  
+│   └── sounds/ (Slurps, crashes)  
+├── index.html  
+├── game.js  
+└── README.md (With funny instructions)
